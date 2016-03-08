c265ae0b7cb1ae47f95e548dd22410a0c014ea3d
Linghein Mattrick <linghein@Lingheins-MacBook-Air.local>
2016-03-07 21:29:41 -0500

    First Commit

---
+++ MedCal X/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MedCal X
+//
+//  Created by Linghein Mattrick on 2016-03-04.
+//  Copyright © 2016 MedCal Tech Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MedCal X/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MedCal X
+//
+//  Created by Linghein Mattrick on 2016-03-04.
+//  Copyright © 2016 MedCal Tech Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
